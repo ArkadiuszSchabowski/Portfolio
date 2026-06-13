691d4c83ad2dfdb5d9b74b547f39e7911276e8e9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-06-13 16:01:03 +0200

    refactor: move about me section to separate component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, HomeComponent],
+  imports: [AboutMeComponent, HeroComponent, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
